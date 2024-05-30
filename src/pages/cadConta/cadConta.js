c86a7e23230ac131685d6879f894e0d773585204
Heitor-Grande <heitorogrande2001@gmail.com>
2024-05-30 13:46:50 -0300

    pre cadastro usuario

--- src/pages/cadConta/cadConta.js
+++ src/pages/cadConta/cadConta.js
@@ -26,6 +26,57 @@ function CadConta() {
                 </nav>
             </div>
 
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                        <p className="text-center"><b>Pré-Cadastro</b></p>
+                        <p className="text-center mb-0">No pré-cadastro solicitamos apenas informações necessarias para que consiga acessar o FIG.</p>
+                        <p className="text-center mb-0">Outras informações de cadastro podem ser solicitadas durante o uso do App.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card mt-5 w-75 m-auto">
+                <div className="card-body">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm col-md-5 col-lg-3">
+                                <div className="form-group">
+                                    <label>Nome</label>
+                                    <input type="email" className="form-control form-control-sm text-capitalize" placeholder="Nome completo" />
+                                </div>
+                            </div>
+                            <div className="col-sm col-md-7 col-lg-3">
+                                <div className="form-group">
+                                    <label>Email</label>
+                                    <input type="email" className="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="exemplo@email.com" />
+                                </div>
+                            </div>
+                            <div className="col-sm col-md-6 col-lg-3">
+                                <div className="form-group">
+                                    <label>Senha</label>
+                                    <input type="password" className="form-control form-control-sm" placeholder="*******" />
+                                </div>
+                            </div>
+                            <div className="col-sm col-md-6 col-lg-3">
+                                <div className="form-group">
+                                    <label>Confirmar Senha</label>
+                                    <input type="password" className="form-control form-control-sm" placeholder="*******" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card-footer">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm col-md col-lg-6 text-center m-auto">
+                                <button className="btn btn-outline-primary btn-sm w-100">Finalizar Pré-Cadastro</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
             <Footer />
